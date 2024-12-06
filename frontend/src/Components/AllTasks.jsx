8644ffec9d49ba2f89169b129b53175a0ae19ab2
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-12-06 18:16:03 +0530

    Update typo

--- frontend/src/Components/AllTasks.jsx
+++ frontend/src/Components/AllTasks.jsx
@@ -22,7 +22,7 @@ export const AllTasks = () => {
         const fetchedUserResponse =  await fetch('https://task-management-system-backend-98ub.onrender.com/api/user/get-user', {
           method: "GET",
           headers: {
-            'content-type': 'appliation/json',
+            'content-type': 'application/json',
              'Authorization': `Bearer ${token}`
           },
         })
